c30c242745ae8134be63aafc18e45e852e0acd98
Mlenaa <milena.arroyo.quinonez@utelvt.edu.ec>
2022-09-30 10:43:56 -0500

    version2

---
+++ suma.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+
+	float x, y, z;
+	cout<< " Ingreso 2 numero xy: " ;
+	cin>>x>>y;
+	z=x+y;
+	cout<< " El resultado es: " ;
+	cout<<z<<endl;
+
+
+    return 0;
+}
