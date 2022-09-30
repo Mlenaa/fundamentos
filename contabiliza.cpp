81302139db9fbe47b54fe6f06294b85cd8bb0c38
Mlenaa <milena.arroyo.quinonez@utelvt.edu.ec>
2022-09-30 12:09:23 -0500

    version2

--- contabiliza.cpp
+++ contabiliza.cpp
@@ -3,20 +3,18 @@ using namespace std;
 
 int main()
 {
-	int i=0,l;
-	float x,s=0;
-	cout<<"ingrese el limite l="; cin>>l;
+
+int MLAQ_i=0,MLAQ_l;
+	float MLAQ_x,MLAQ_s=0;
+	cout<<"ingrese el limite MLAQ_l="; cin>>MLAQ_l;
 	do{
 
-	cout<<"ingrese el numero x="; cin>>x;
-	i=i+1;
-	s=s+x;
+	cout<<"ingrese el numero MLAQ_x="; cin>>MLAQ_x;
+	MLAQ_i=MLAQ_i+1;
+	MLAQ_s=MLAQ_s+MLAQ_x;
 
 
 
-	}while(i<l);
-	cout<<"Se ingresaron "<<l<<" numeros "<< "que sumaron "<<s<<endl;
-	return 0;
-
-
-}
+	}while(MLAQ_i<MLAQ_l);
+	cout<<"Se ingresaron "<<MLAQ_l<<" numeros "<< "que sumaron "<<MLAQ_s<<endl;
+	return 0;}
